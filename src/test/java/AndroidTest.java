47269eadb77db2d176f20e308df7a9a11fb993eb
Surajit Sarkar <suro@Surajits-MacBook-Pro-2.local>
2023-12-24 18:02:16 +0530

    Anddroid basic test

--- src/test/java/AndroidTest.java
+++ src/test/java/AndroidTest.java
@@ -1,6 +1,8 @@
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.remote.AutomationName;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -9,7 +11,7 @@ import java.net.URL;
 public class AndroidTest {
 
     @Test
-    public void androidLaunchTest() throws MalformedURLException {
+    public void androidLaunchTest() throws MalformedURLException, InterruptedException {
         UiAutomator2Options options = new UiAutomator2Options();
         options.setPlatformName("Android");//optional
         options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);//optional
@@ -17,6 +19,14 @@ public class AndroidTest {
         options.setApp(System.getProperty("user.dir")+"/apps/Android-MyDemoAppRN.1.3.0.build-244.apk");
 
         AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"),options);
+
+        driver.findElement(AppiumBy.accessibilityId("open menu")).click();
+        driver.findElement(AppiumBy.accessibilityId("menu item log in")).click();
+        driver.findElement(AppiumBy.accessibilityId("Username input field")).sendKeys("bob@example.com");
+        driver.findElement(AppiumBy.accessibilityId("Password input field")).sendKeys("10203040");
+        driver.findElement(AppiumBy.accessibilityId("Login button")).click();
+        driver.close();
+        driver.quit();
 
 
 
